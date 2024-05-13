4356331278aeca2f2404b02451d0a5f2ced58a35
Hihthei <cbouillon@et.esiea.fr>
2024-05-13 16:13:38 +0200

    IFNDEF

--- TPF_TP_Sem-4/Settings.h
+++ TPF_TP_Sem-4/Settings.h
@@ -1,23 +1,24 @@
-#pragma once
+#ifndef SETTINGS_H
+#define SETTINGS_H
 
-#define _CRT_SECURE_NO_WARNINGS
+    #include <assert.h>
+    #include <math.h>
+    #include <stdbool.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <stdint.h>
+    #include <time.h>
 
-#include <assert.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <time.h>
+    #define AssertNew(ptr) { if (ptr == NULL) { assert(false); abort(); } }
 
-#define AssertNew(ptr) { if (ptr == NULL) { assert(false); abort(); } }
+    #ifdef _MSC_VER
+    #  define INLINE inline
+    #else
+    #  define INLINE static inline
+    #endif
 
-#ifdef _MSC_VER
-#  define INLINE inline
-#else
-#  define INLINE static inline
+    INLINE int minInt(int a, int b) { return a < b ? a : b; }
+    INLINE int maxInt(int a, int b) { return a > b ? a : b; }
+
 #endif
-
-INLINE int minInt(int a, int b) { return a < b ? a : b; }
-INLINE int maxInt(int a, int b) { return a > b ? a : b; }
