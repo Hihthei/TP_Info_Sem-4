c57574267fa3f7bb7e390e6374b66f3d6fb93520
Hihthei <cbouillon@et.esiea.fr>
2024-05-13 16:16:09 +0200

    main.c

--- TPF_TP_Sem-4/Main.c
+++ TPF_TP_Sem-4/Main.c
@@ -4,6 +4,20 @@
 #include "ShortestPath.h"
 
 int main() {
+    //TIME CLOCK INITIALISATION --------------------------------
+    clock_t start = 0, middle = 0, end = 0;
+    double cpu_time_used = 0;
+    start = clock();
+    //----------------------------------------------------------
+
+
+
+    //TIME CLOCK END --------------------------------------------
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("____________________________\n"
+        "\nTemps d'execution : %.3fs.\n", cpu_time_used);
+    //----------------------------------------------------------
 
     return EXIT_SUCCESS;
 }
